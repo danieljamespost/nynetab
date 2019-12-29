264aed1adc9300c8680e7827c956b82f0524c5aa
Daniel <daniel@martin.ai>
2019-12-28 23:48:51 -0500

    tab expand working properly

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"unicode/utf8"
 
 	"9fans.net/go/acme"
 )
@@ -18,35 +19,44 @@ func main() {
 	if err != nil {
 		log.Print(err)
 	}
-	for e := range events(w) {
-		fmt.Printf("%+v\n", e)
-
-	}
+	expandtab(w)
 }
 
-func events(w *acme.Win) <-chan string {
-	c := make(chan string, 10)
-	go func() {
-		for e := range w.EventChan() {
-			evtType := fmt.Sprintf("%d%d", e.C1, e.C2)
-			switch (evtType) {
-			case "7573":	// KI
-    				if string(e.Text) == "	" {
-					fmt.Println("TAB")
-	// 				fmt.Printf("%+v\n", e)
-					err := w.Addr("#%d+#1", e.Q0)
-					if err != nil {
-						log.Print(err)
-					}
-					w.Write("data", []byte("    "))
-					break
+func expandtab(w *acme.Win) {
+	for e := range w.EventChan() {
+		fmt.Printf("%+v\n", e)
+		fmt.Printf("'%s'\n", string(e.Text))
+
+		evtType := fmt.Sprintf("%s%s", string(e.C1), string(e.C2))
+		switch (evtType) {
+		case "KI":
+    			if string(e.Text) == "	" {
+				tab := []byte("    ")
+				err := w.Addr("#%d;+#1", e.Q0)
+				if err != nil {
+					log.Print(err)
 				}
-			default:
+				w.Write("data", tab)
+
+				e.C1 = 70
+				e.C2 = 73
+				e.Q1 = e.Q0 + utf8.RuneCount(tab)
+				e.OrigQ1 = e.Q0 + utf8.RuneCount(tab)
+				e.Nr = utf8.RuneCount(tab)
+				e.Text = tab
 				w.WriteEvent(e)
+
+				fmt.Println("TAB")
+				fmt.Printf("%+v\n", e)
 			}
+		case "MX":
+			fallthrough
+		case "Mx":
+			fallthrough
+		case "Ml":
+			fallthrough
+		case "ML":
+			w.WriteEvent(e)
 		}
-		w.CloseFiles()
-		close(c)
-	}()
-	return c
+	}
 }
